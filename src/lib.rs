d08463d856d1bd5fd731a028bd55d54f1352fe93
Magnus Ulimoen <magnus@ulimoen.dev>
2024-06-22 08:05:01 +0200

    Link C++

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! ```bash
 //! cargo build --features bindgen
 //! ```
-//! 
+//!
 //! ## Licence
 //! `snappy_src` is licensed under either of
 //!  - the Apache License, Version 2.0 [LICENSE-APACHE](./LICENCE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0> or
@@ -18,5 +18,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+
+extern crate link_cplusplus;
 
 include!(concat!(env!("CARGO_MANIFEST_DIR"), "/bindings.rs"));
